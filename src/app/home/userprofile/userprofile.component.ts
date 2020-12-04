69072157f7f7470c64c1ed2bad382103f1beee8e
sitcomdeveloper <sitcomdeveloper43@gmail.com>
2020-12-04 18:35:06 +0530

    Rohan 04-12-2020

--- src/app/home/userprofile/userprofile.component.ts
+++ src/app/home/userprofile/userprofile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userprofile',
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserprofileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
   logout() {
-
+    this.router.navigateByUrl('/home');
   }
 }
